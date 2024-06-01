811920883af4043b86a14f09d4280bb5c64340be
Martin <frostwol91@gmail.com>
2024-06-01 15:04:08 +0200

    First commit

---
+++ src/main/java/com/bigfoot/tenantmonitor/TenantMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.bigfoot.tenantmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenantMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigfoot/tenantmonitor/TenantMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigfoot.tenantmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
