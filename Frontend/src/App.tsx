9f304d570aa86bdb6c25ad496bca6e03865ae85a
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2024-06-08 22:47:22 +0200

    Navbar tutorial finished, it works like a charm, only need to implement the actual application :)

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,23 @@
 import Navbar from "./components/Navbar"
 import "./style.css"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import About from "./pages/About"
 
 function App() {
-    console.log(window.location.pathname)
-    return <Navbar />
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pricing" element={<Pricing />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </>
+    )
 }
 
 export default App
