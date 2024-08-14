4da6b5a6b9e1533f024c3028a1f0422a72dd68fa
Martin Piko <piko.martin99@gmail.com>
2024-08-14 19:06:01 +0200

    Added a bunch of things...
    Tried to create the authentication mechanism, however I'm struggling with only enabling only vaading related endpoints. Also, extended the backend service with refresh token capabilities.

---
+++ Backend/src/main/java/com/bigfoot/tenantmonitor/client/pages/EndpointTestPage.java
@@ -0,0 +1,32 @@
+package com.bigfoot.tenantmonitor.client.pages;
+
+import com.bigfoot.tenantmonitor.client.BackendService;
+import com.bigfoot.tenantmonitor.client.layout.BaseLayout;
+import com.bigfoot.tenantmonitor.dto.PropertyDTO;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import java.util.List;
+
+@Route(value = "endpointTest", layout = BaseLayout.class)
+public class EndpointTestPage extends VerticalLayout {
+
+    private final BackendService backendService;
+
+    public EndpointTestPage(BackendService backendService){
+        this.backendService = backendService;
+        H1 title = new H1("Endpoint test buttons");
+        VerticalLayout propertiesList = new VerticalLayout();
+        Button getProperties = new Button("Get properties");
+        getProperties.addClickListener(e -> {
+            List<PropertyDTO> properties = backendService.getProperties();
+            properties.forEach(p -> propertiesList.add(new H2(p.toString())));
+        });
+
+
+        add(title, getProperties, propertiesList);
+    }
+}
