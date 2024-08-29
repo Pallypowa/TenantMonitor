0320eafccc17f13df731402ddb3a12b90e882628
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2024-08-29 22:31:37 +0200

    Removed root frontend directory.

--- Frontend/src/contexts/AuthContext.js
+++
@@ -1,9 +0,0 @@
-const AuthContext = React.createContext()
-
-export function useAuth() {
-    return React.useContext(AuthContext)
-}
-
-export function AuthProvider({ children }) {
-    return <AuthContext.Provider>{children}</AuthContext.Provider>
-}
