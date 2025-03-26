9eaf473ecec8cdd5e010a5e6436faa06d4e84a8f
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2025-03-26 22:14:11 +0100

    removed cors from usercontroller.

--- src/main/java/com/bigfoot/tenantmonitor/controller/UsersController.java
+++ src/main/java/com/bigfoot/tenantmonitor/controller/UsersController.java
@@ -4,6 +4,7 @@ import com.bigfoot.tenantmonitor.dto.UserDTO;
 import com.bigfoot.tenantmonitor.service.AuthService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,12 @@ import java.security.Principal;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "jwt")
+@CrossOrigin(origins = "*", allowedHeaders = "*") //TODO
 public class UsersController {
 
     private final AuthService authService;
 
-    @GetMapping("/")
+    @GetMapping()
     public UserDTO getUserInfo(Principal principal) {
         return authService.getUserInfo(principal.getName());
     }
